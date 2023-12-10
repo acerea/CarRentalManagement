6fb77bfc10df2529be49b39e72c1c7cca6cf2b3d
Alliana Rial <raejamero@gmail.com>
2023-12-10 18:32:54 +0800

    Revert "Added REST API Endpoints and Unit Of Work pattern."
    This reverts commit 667e5f10e67dc9eb73a24e6eced692ec3d8a0100.

--- Server/Program.cs
+++ Server/Program.cs
@@ -4,8 +4,6 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
-using CarRentalManagement.Server.IRepository;
-using CarRentalManagement.Server.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,8 +21,6 @@ builder.Services.AddIdentityServer()
 
 builder.Services.AddAuthentication()
     .AddIdentityServerJwt();
-
-builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
