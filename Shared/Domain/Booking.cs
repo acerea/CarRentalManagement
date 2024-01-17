63affa120804d200d0ea267208e58ac66e2634ca
Alliana Rial <raejamero@gmail.com>
2024-01-17 16:23:55 +0800

    Up to Practice 123 Created: FormComponent and Edit .razor to all features + BackToList @ components
    Modified: All Indexees to change the Edit Button to ___/edit/id from ___/view/id + Program.cs for nuget package + Booking @ Shared.Domain for DateTime DateIn

--- Shared/Domain/Booking.cs
+++ Shared/Domain/Booking.cs
@@ -1,12 +1,31 @@
-﻿namespace CarRentalManagement.Shared.Domain
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarRentalManagement.Shared.Domain
 {
-    public class Booking : BaseDomainModel
-    {
-        public DateTime DateOut { get; set; }
-        public DateTime DateIn { get; set; }
-        public int VehicleId { get; set; }
-        public virtual Vehicle? Vehicle { get; set; }
-        public int CustomerId { get; set; }
-        public virtual Customer? Customer { get; set; }
-    }
+	public class Booking : BaseDomainModel, IValidatableObject
+	{
+		public DateTime DateOut { get; set; }
+		public DateTime? DateIn { get; set; }
+		public int VehicleId { get; set; }
+		public virtual Vehicle? Vehicle { get; set; }
+		public int CustomerId { get; set; }
+		public virtual Customer? Customer { get; set; }
+
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			if (DateIn != null)
+			{
+				if (DateIn <= DateOut)
+				{
+					yield return new ValidationResult("DateIn must be greater than DateOut", new[] { "DateIn" });
+				}
+			}
+		}
+
+
+	}
+
 }
